668c285e1763e538fa4edfd50ed3e50d2ca64a19
Vinit Bansal <vinitbansal1998@gmail.com>
2021-10-13 01:24:14 +0530

    Layout Modules and Components Generated

---
+++ src/app/shared/layout/layout.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LayoutRoutingModule } from './layout-routing.module';
+import { LayoutComponent } from './layout.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavComponent } from './nav/nav.component';
+
+
+@NgModule({
+  declarations: [
+    LayoutComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent
+  ],
+  imports: [
+    CommonModule,
+    LayoutRoutingModule
+  ]
+})
+export class LayoutModule { }
